a4369185819b1b9a83c829867d322e377fa4b709
Yaasir Rahmat Kautsar <96896573+4SSRahmadd@users.noreply.github.com>
2023-10-13 22:40:30 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/login.dart';
-import 'package:flutter_application_1/home.dart';
+import 'login.dart';
+
 
 void main() {
   runApp(const MyApp());
